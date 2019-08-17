ffa850a8cb192a13608bd49cd9668a9d580bb779
cofuente <contact@cofuente.io>
2019-08-17 19:44:53 -0400

    Updates

---
+++ precache-manifest.d44892d07908887f6e5db645c9700bea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3acbc23692a967723b04b89851867f7",
+    "url": "/cofue-sensor-tests/index.html"
+  },
+  {
+    "revision": "e171c66c2e09ac0d1a5f",
+    "url": "/cofue-sensor-tests/static/css/main.1cde2322.chunk.css"
+  },
+  {
+    "revision": "9a58da8049fce9c6d0d6",
+    "url": "/cofue-sensor-tests/static/js/2.695f8dd0.chunk.js"
+  },
+  {
+    "revision": "e171c66c2e09ac0d1a5f",
+    "url": "/cofue-sensor-tests/static/js/main.ded138b2.chunk.js"
+  },
+  {
+    "revision": "778221df965f13a6e15f",
+    "url": "/cofue-sensor-tests/static/js/runtime~main.72a8d5d0.js"
+  },
+  {
+    "revision": "cad8bdb13ca276852daa5be3126a44df",
+    "url": "/cofue-sensor-tests/static/media/card.cad8bdb1.png"
+  },
+  {
+    "revision": "1b45953d0f68a73c3a89018cfa770699",
+    "url": "/cofue-sensor-tests/static/media/resume.1b45953d.pdf"
+  }
+]);
